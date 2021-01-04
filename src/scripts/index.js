56a22b95d965e3572eb36db598bd05607dedbbff
dwikyls <kreuzvernicterlanze@gmail.com>
2021-01-05 05:56:37 +0700

    add: restaurants list

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,9 @@
-import 'regenerator-runtime'; /* for async await transpile */
+import 'regenerator-runtime';
 import '../styles/main.css';
+import '../styles/responsive.css'
+import data from '../DATA.json'
 
+//SIDEBAR
 const sideBar = document.getElementById('header_menu')
 const drawer = document.getElementById('drawer')
 const main = document.getElementById('main')
@@ -13,4 +16,26 @@ sideBar.addEventListener("click", event => {
 main.addEventListener("click", event => {
     drawer.classList.remove("open");
     event.stopPropagation();
+})
+
+//RESTAURAT ITEM LIST
+const restList = document.querySelector('.rest-list')
+let restData = data.restaurants
+
+restData.forEach(e => {
+    restList.innerHTML += `
+        <div class="card">
+            <div class="card-header">
+                <div class="city-tag">
+                    <b tabindex="0">Rating: ${e.rating}</b>
+                </div>
+                <img width="100%" src="${e.pictureId}" alt="gambar restaurant ${e.name}">
+            </div>
+            <div class="card-body">
+                <b tabindex="0">Lokasi: ${e.city}</b>
+                <h3 tabindex="0">${e.name}</h3>
+                <p tabindex="0">${e.description}</p>
+            </div>
+        </div>
+    `
 })
