e6053f52d654ea7a6a4b3c337b3e14e428ddd172
dwikyls <kreuzvernicterlanze@gmail.com>
2021-01-04 16:48:19 +0700

    add: navbar, banner, content

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,16 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
-console.log('Hello Coders! :)');
+const sideBar = document.getElementById('header_menu')
+const drawer = document.getElementById('drawer')
+const main = document.getElementById('main')
+
+sideBar.addEventListener("click", event => {
+    drawer.classList.toggle("open");
+    event.stopPropagation();
+});
+
+main.addEventListener("click", event => {
+    drawer.classList.remove("open");
+    event.stopPropagation();
+})
